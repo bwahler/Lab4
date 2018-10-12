00e30519731e8f9728f899f43c57ec216f40dc5f
Brian <bmwahler@gmail.com>
2018-10-11 21:31:40 -0400

    lab4

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab_4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int Num, i;
+            do
+            {
+
+                Console.WriteLine("Please enter a number:");
+                Num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Number  Squared Cubed");
+                Console.WriteLine("======= ======= =======");
+
+                for (i = 1; i <= Num; i++)
+                {
+                    Console.WriteLine("{0}{1}{2}{3}{4}", i, "    \t", Math.Pow(i, 2), "    \t", Math.Pow(i, 3));
+                }
+                Console.WriteLine("Would you like to continue? (y/n)");
+            }
+            while (Console.ReadLine() == "y");
+
+            if (Console.ReadLine() == "n")
+                return;               
+        }
+    }
+}
+
